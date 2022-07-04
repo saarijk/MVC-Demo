fc58f98c7ddf79921e56ded114b7f93480341b39
jsaari <jsaari@student.oulu.fi>
2022-07-04 12:44:05 +0300

    Initial project

---
+++ MVC_KatDemo/MVC_KatDemo/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_KatDemo.Models;
+
+namespace MVC_KatDemo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //url: Movies/Random
+        public IActionResult Random()
+        {
+            //normally we'd retrieve this information from a database
+            var movie = new Movie() { nName = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ MVC_KatDemo/MVC_KatDemo/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_KatDemo.Models
+{
+    public class Movie
+    {
+        //this class only has a couple properties that represent state
+        //to quickly create a property long press tab
+        public int nId { get; set; }
+        public string nName { get; set; }
+    }
+
+}
