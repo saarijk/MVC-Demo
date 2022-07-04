ccf608572ace50fb013fa6b66af231eeea28582f
jsaari <jsaari@student.oulu.fi>
2022-07-04 20:17:14 +0300

    Added theme (bootswatch)

--- MVC_KatDemo/MVC_KatDemo/Models/Movie.cs
+++ MVC_KatDemo/MVC_KatDemo/Models/Movie.cs
@@ -7,8 +7,7 @@ namespace MVC_KatDemo.Models
 {
     public class Movie
     {
-        //this class only has a couple properties that represent state
-        //to quickly create a property long press tab
+
         public int nId { get; set; }
         public string nName { get; set; }
     }
--- MVC_KatDemo/MVC_KatDemo/Startup.cs
+++ MVC_KatDemo/MVC_KatDemo/Startup.cs
@@ -29,6 +29,7 @@ namespace MVC_KatDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+         
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
